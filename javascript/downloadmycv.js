f00829c0223c2c981da5f8ba900ee2962cd79794
ahmadbamallee <112807539+ahmadbamallee@users.noreply.github.com>
2023-08-01 22:23:50 +0400

    initial commit

---
+++ javascript/downloadmycv.js
@@ -0,0 +1,11 @@
+function downloadFile() {
+    const cvUrl = "assets/mycv/cv.pdf";
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = "cv.pdf";
+    link.target = "_blank";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  
