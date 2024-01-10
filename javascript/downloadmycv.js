e201f5cdfc1dc4d8f69dc204ae455597c38f29c8
ahmadbamallee <112807539+ahmadbamallee@users.noreply.github.com>
2024-01-10 16:36:42 +0400

    Update downloadmycv.js

--- javascript/downloadmycv.js
+++ javascript/downloadmycv.js
@@ -1,11 +1,11 @@
 function downloadFile() {
   // Ask the user for confirmation
-  var confirmation = confirm("Are you sure you want to download my CV?");
+  var confirmation = confirm("Are you sure you want to download my CV ?");
   // If the user confirms, proceed with the download
   if (confirmation) {
     // Prevent the default behavior of the button click
     event.preventDefault();
-    // Create a delay of 5 seconds (5000 milliseconds)
+    // Create a delay of 3 seconds (5000 milliseconds)
     setTimeout(function () {
       // Hard code the CV URL
       const cvUrl = "assets/mycv/Ahmad_Abubakar_CV.pdf";
@@ -13,7 +13,7 @@ function downloadFile() {
       const link = document.createElement("a");
       // Set the link attributes
       link.href = cvUrl;
-      link.download = "cv.pdf";
+      link.download = "Ahmad_cv.pdf";
       link.target = "_blank";
       // Append the link to the document body
       document.body.appendChild(link);
